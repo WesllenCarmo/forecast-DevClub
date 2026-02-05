9a343d7bf3764ef5a17e8381e45d3a157c143681
Wesllen <wesllenaraujo.developer@gmail.com>
2026-02-04 18:48:13 -0800

    Introduced useRef to catch the city's name in the input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useRef } from 'react'
 import './App.css'
 
-function App() {
 
+function App() {
+  const inputRef = useRef()
+  function searchCity() {
+    console.log(inputRef.current.value)
+  }
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>DevClub Previsão do Tempo</h1>
+      <input ref={inputRef}type="text" placeholder='Digite o nome da cidade:'/>
+      <button onClick={searchCity}>Buscar</button>
+    </div>
   )
 }
 
