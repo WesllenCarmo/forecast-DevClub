015b634d3e6455204f0a6387fbf9d508a6b7ddeb
Wesllen <wesllenaraujo.developer@gmail.com>
2026-02-05 18:30:21 -0800

    Axios installed. searchCity function, which collect information from the API, created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useRef } from 'react'
+import axios from 'axios'
 import './App.css'
 
 
 function App() {
   const inputRef = useRef()
-  function searchCity() {
-    console.log(inputRef.current.value)
+
+  async function searchCity() {
+
+    const city = inputRef.current.value
+    const key = "7703f049f8659d595b920d2eb128d0c1"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=pt_br`
+
+    const data = await axios.get(url)
+    console.log(data);
+    
+    
   }
   return (
     <div>
