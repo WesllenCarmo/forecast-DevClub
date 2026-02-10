607954b27903cc2c1df27a35ff49124c54d4df07
Wesllen <wesllenaraujo.developer@gmail.com>
2026-02-09 18:12:53 -0800

    Add 'feels like', 'humidity', and 'pressure' sections with their respective datas.

--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -12,6 +12,12 @@ function WeatherInformations({ forecast }) {
                 <img src={`https://openweathermap.org/payload/api/media/file/${forecast.weather[0].icon}.png`} alt="Weather Icon" />
                 <p>{Math.round(forecast.main.temp)}°C</p>
             </div>
+            <p>{forecast.weather[0].description}</p>
+            <div>
+                <p>Sensação térmica: {Math.round(forecast.main.feels_like)}°C</p>
+                <p>Umidade: {Math.round(forecast.main.humidity)}%</p>
+                <p>Pressão: {Math.round(forecast.main.pressure)} hPa</p>
+            </div>
         </div>
     )
 }
