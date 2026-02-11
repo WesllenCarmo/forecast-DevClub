278fefdec22cbfbc4e252fad659d31167a5e96e8
Wesllen <wesllenaraujo.developer@gmail.com>
2026-02-10 18:15:21 -0800

    Add className in App.jsx div. Created stylesheet in index.css and App.css files.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     
   }
   return (
-    <div>
+    <div className='container'>
       <h1>DevClub Previsão do Tempo</h1>
       <input ref={inputRef}type="text" placeholder='Digite o nome da cidade:'/>
       <button onClick={searchCity}>Buscar</button>
