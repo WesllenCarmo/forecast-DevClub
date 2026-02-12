edab2e953b7b1bb65acae1df6bc53fa74653a2d9
Wesllen <wesllenaraujo.developer@gmail.com>
2026-02-11 18:43:25 -0800

    Add classes on WeatherInformations.jsx elements. Add styles for the main forecast datas.

--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -1,19 +1,20 @@
+import styles from './WeatherInformations.module.css'
 function WeatherInformations({ forecast }) {
     if (!forecast.weather) {
         return null;
     }
     
     return (
-        <div>
-            <div>
+        <div className={styles.weatherContainer}>
+            <div className={styles.weatherInfo}>
                 <h2>{forecast.name}</h2>
             </div>
-            <div>
+            <div className={styles.weatherInfo}>
                 <img src={`https://openweathermap.org/payload/api/media/file/${forecast.weather[0].icon}.png`} alt="Weather Icon" />
-                <p>{Math.round(forecast.main.temp)}°C</p>
+                <p className={styles.temperature}>{Math.round(forecast.main.temp)}°C</p>
             </div>
-            <p>{forecast.weather[0].description}</p>
-            <div>
+            <p className={styles.description}>{forecast.weather[0].description}</p>
+            <div className={styles.details}>
                 <p>Sensação térmica: {Math.round(forecast.main.feels_like)}°C</p>
                 <p>Umidade: {Math.round(forecast.main.humidity)}%</p>
                 <p>Pressão: {Math.round(forecast.main.pressure)} hPa</p>
