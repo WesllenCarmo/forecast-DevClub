9a01f6b2c16e154544194ff5cf6724d033377db3
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-02-17 19:03:51 -0800

    Add informations of a five days forecast.

--- src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
+++ src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -3,7 +3,6 @@ function WeatherInformations5Days({ forecast5Days }) {
     if (!forecast5Days) {
         return null;
     }
-    console.log(forecast5Days);
     let dailyForecast = {}
 
     for(let forecast of forecast5Days.list) {
@@ -14,11 +13,26 @@ function WeatherInformations5Days({ forecast5Days }) {
         }
 
     }
-    console.log(dailyForecast);
+    // Pega os próximos 5 dias, ignorando o dia atual
+    const nextFiveDays = Object.values(dailyForecast).slice(1,6)
+    // converte a data para o formato "[dia da semana], [dia do mês]"
+    function convertDate(date) {
+        const newDate = new Date(date * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit'})
+        return newDate;
+    }
     
     return (
+        // exibe as informações da previsão para os próximos 5 dias
         <div className={styles.weatherContainer}>
-            <p>Forecast 5 Days</p>
+            <p>Previsão dos Próximos 5 Dias</p>
+            {nextFiveDays.map(forecast => (
+                <div key={forecast.dt}>
+                    <p>{convertDate(forecast.dt)}</p>
+                    <img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt={forecast.weather[0].description} />
+                    <p>{forecast.weather[0].description}</p>
+                    <p>{Math.round(forecast.main.temp_min)}°C / {Math.round(forecast.main.temp_max)}°C</p>
+                </div>
+            ))}
         </div>
     )
 }
