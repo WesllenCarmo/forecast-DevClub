0dc6a76bc058f8436c091ac09fb1ec9ce7552f63
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-02-23 23:05:05 -0800

    Solve misunderstanding about four days forecast instead of five.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useRef } from 'react'
 import axios from 'axios'
 import WeatherInformations from '../src/components/WeatherInformations/WeatherInformations.jsx'
-import WeatherInformations5Days from './components/WeatherInformations5Days/WeatherInformations5Days.jsx'
+import WeatherInformations4Days from './components/WeatherInformations4Days/WeatherInformations4Days.jsx'
 import './App.css'
 
 
 function App() {
   const [weather, setWeather] = useState()
-  const [weather5Days, setWeather5Days] = useState()
+  const [weather4Days, setWeather4Days] = useState()
   const inputRef = useRef()
 
   async function searchCity() {
@@ -16,12 +16,12 @@ function App() {
     const key = import.meta.env.VITE_OPENWEATHER_API_KEY
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=pt_br`
-    const url5dayforecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric&lang=pt_br`
+    const url4dayforecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric&lang=pt_br`
 
     const apiInfo = await axios.get(url)
-    const apiInfo5DayForecast = await axios.get(url5dayforecast)
+    const apiInfo4DayForecast = await axios.get(url4dayforecast)
 
-    setWeather5Days(apiInfo5DayForecast.data)
+    setWeather4Days(apiInfo4DayForecast.data)
     setWeather(apiInfo.data)
   }
   return (
@@ -31,7 +31,7 @@ function App() {
       <button onClick={searchCity}>Buscar</button>
 
       {weather && <WeatherInformations forecast={weather}/>}
-      {weather5Days && <WeatherInformations5Days forecast5Days={weather5Days}/>}
+      {weather4Days && <WeatherInformations4Days forecast4Days={weather4Days}/>}
     </div>
   )
 }
--- src/components/WeatherInformations4Days/WeatherInformations4Days.jsx
+++ src/components/WeatherInformations4Days/WeatherInformations4Days.jsx
@@ -1,11 +1,11 @@
-import styles from './WeatherInformations5Days.module.css'
-function WeatherInformations5Days({ forecast5Days }) {
-    if (!forecast5Days) {
+import styles from './WeatherInformations4Days.module.css'
+function WeatherInformations4Days({ forecast4Days }) {
+    if (!forecast4Days) {
         return null;
     }
     let dailyForecast = {}
 
-    for(let forecast of forecast5Days.list) {
+    for(let forecast of forecast4Days.list) {
         const date = new Date(forecast.dt * 1000).toLocaleDateString()
 
         if (!dailyForecast[date]) {
@@ -13,7 +13,7 @@ function WeatherInformations5Days({ forecast5Days }) {
         }
 
     }
-    const nextFiveDays = Object.values(dailyForecast).slice(1,6)
+    const nextFourDays = Object.values(dailyForecast).slice(1,5)
     function convertDate(date) {
         const newDate = new Date(date * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit'})
         return newDate;
@@ -21,9 +21,9 @@ function WeatherInformations5Days({ forecast5Days }) {
     
     return (
         <div className={styles.weatherContainer}>
-            <h3>Previsão dos Próximos 5 Dias</h3>
+            <h3>Previsão dos Próximos 4 Dias</h3>
             <div className={styles.weatherList}>
-                {nextFiveDays.map(forecast => (
+                {nextFourDays.map(forecast => (
                     <div key={forecast.dt} className={styles.weatherItem}>
                         <p className={styles.forecastDay}>{convertDate(forecast.dt)}</p>
                         <img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt={forecast.weather[0].description} />
@@ -35,4 +35,4 @@ function WeatherInformations5Days({ forecast5Days }) {
         </div>
     )
 }
-export default WeatherInformations5Days;
+export default WeatherInformations4Days;
